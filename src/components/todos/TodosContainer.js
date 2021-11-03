39f3b81308345e315247bbe9cd356028bd669860
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-03 13:20:03 -0700

    commit tree 4623db48cfda41f6b193c82c1b31e52290551349

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import Todo from "./Todo";
+
+class TodosContainer extends React.Component {
+  renderTodos = () =>
+    this.props.todos.map((todo, id) => <Todo key={id} text={todo} />);
+
+  render() {
+    console.log(this.props.todos);
+    return <ol>{this.renderTodos()}</ol>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos,
+  };
+};
+
+export default connect(mapStateToProps)(TodosContainer);
